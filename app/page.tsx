21d5c5770ef168def88c255f9c1ffa6784817ae2
novice2030 <148373504+novice2030@users.noreply.github.com>
2023-10-19 08:58:39 +0530

    removed videos

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Videos from "@/components/videos";
 import Exihibition from "@/components/exhibition";
-import Test from "@/components/test";
 
 export default async function Home() {
   return (
@@ -13,19 +12,12 @@ export default async function Home() {
       <Intro />
       <SectionDivider />
       <About />
-      {/* <Projects />
-      <div className="my-10"></div>
-      <Videos />
-      <div className="my-10"></div> */}
       <Projects />
       <div className="my-10"></div>
-      <Videos />
+      {/* <Videos /> */}
       <div className="my-10"></div>
       <Exihibition />
       <div className="my-10"></div>
-
-      {/* <Skills /> */}
-      {/* <Experience /> */}
       <Contact />
     </main>
   );
